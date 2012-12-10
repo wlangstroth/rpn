b1b51784cfd93706967006a98448cc275a040c7d
Will Langstroth <will@langstroth.com>
2012-12-10 15:33:27 -0500

    add simple tests

--- tests/Main.hs
+++ tests/Main.hs
@@ -14,11 +14,43 @@ main :: IO ()
 main = defaultMain tests
 
 tests :: [Test]
-tests = [ testMultiply ]
+tests = [ testMultiplication
+        , testAddition
+        , testSubtraction
+        , testDivision
+        ]
 
-testMultiply :: Test
-testMultiply = testCase "Multiplication" $ do
-    assertEqual "Multiply 3 and 6" result shouldBe
+testMultiplication :: Test
+testMultiplication = testCase "Multiplication" $ do
+    assertEqual "'3 6 *' should equal 18" result shouldBe
   where
     result = solveRPN "3 6 *"
     shouldBe = 18.0
+
+testAddition :: Test
+testAddition = testCase "Addition" $ do
+    assertEqual "'3 6 +' should equal 9" result shouldBe
+  where
+    result = solveRPN "3 6 +"
+    shouldBe = 9.0
+
+testSubtraction :: Test
+testSubtraction = testCase "Subtraction" $ do
+    assertEqual "'3 6 -' should equal -3" result shouldBe
+  where
+    result = solveRPN "3 6 -"
+    shouldBe = -3.0
+
+testDivision :: Test
+testDivision = testCase "Division" $ do
+    assertEqual "'6 2 /' should equal 3" result shouldBe
+  where
+    result = solveRPN "6 2 /"
+    shouldBe = 3.0
+
+testDivision :: Test
+testDivision = testCase "Sum" $ do
+    assertEqual "'3 2 6 2 sum' should equal 13" result shouldBe
+  where
+    result = solveRPN "3 2 6 2 sum"
+    shouldBe = 13.0
